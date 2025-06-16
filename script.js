b2cc1c1061dcf97a78d5b7c7928650408d09eaae
rushin-gh <rushin.mailme@gmail.com>
2025-06-17 00:42:06 +0530

    Integrated CF userrating API to show current rating of user

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 let username = document.getElementById('username');
 let cfrating = document.getElementById('cfsubmit');
-let ccrating = document.getElementById('ccsubmit');
-
-// cf Profile API -> https://codeforces.com/api/user.rating?handle=rushin_cf
-// cc Profile API -> https://codechef-api.vercel.app/handle/rushin_cc
+let ratingDiv = document.getElementById('ratingDiv');
 
 cfrating.addEventListener('click', function(event) {
     let input = username.value;
     if (ValidateInput(input)) {
-        
-    }
-})
-
-ccrating.addEventListener('click', function(event) {
-    let input = username.value;
-    if (ValidateInput(input)) {
-        
+        let profileURL = GetURL(input, 'Codeforces');
+        if (profileURL) {
+            console.log(profileURL);
+            fetch(profileURL)
+            .then(resp => {
+                if (resp.status === 400) {
+                    alert('User not found');
+                    return null;
+                }
+                return resp.json();
+            })
+            .then(json => {
+                let result = json.result.at(-1);
+                // console.log(result);
+                ratingDiv.innerHTML = `Current Codeforces Rating <strong>${result.newRating}</strong>`;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        } else {
+            alert('URL is not formed well');
+        }
     }
 })
 
 function ValidateInput(input) {
-    alert('Enter non-empty username.');
+    if (input === '') {
+        alert('Enter non-empty username.');
+    }
     return input !== '';
+}
+
+function GetURL(username, platform) {
+    if (platform == 'Codeforces') {
+        return `https://codeforces.com/api/user.rating?handle=${username}`;
+    }
+    return '';
 }
